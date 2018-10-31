edad4299840c0467375231b565add9ebe1d239c6
Sebastian Graf <sebastian.graf1@students.unibe.ch>
2018-10-31 23:27:18 +0100

    redis integration example (Feature, #14)

--- models/database.py
+++ models/database.py
@@ -1,3 +1,26 @@
+from redis import *
 
-def create_tables():
-    return ""
+
+class Database:
+
+    __database = None
+
+    def __setup_database(self):
+        if self.__database is None:
+            __database = StrictRedis(host='localhost', port=6379, db=0)
+        return __database
+
+    def insert(self, key, value):
+        if self.__database is None:
+            self.__setup_database()
+        self.__database.set(key, value)
+
+    def get(self, key):
+        if self.__database is None:
+            self.__setup_database()
+        return self.__database.get(key)
+
+    def delete(self, key):
+        if self.__database is None:
+            self.__setup_database()
+        self.__database.remove(key)
