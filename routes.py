2ec18d0dc1cf5b01ea2bf7ba427ee6dd989ffb6e
rniem379349 <robert.niemiec95@wp.pl>
2018-11-22 17:36:21 +0100

    Add resources, routes for gas, humidity, pressure TG-32 #in-progress

--- routes.py
+++ routes.py
@@ -2,7 +2,9 @@ import aiohttp_cors
 import views
 import controllers.user
 import controllers.temperature
-
+import controllers.pressure
+import controllers.humidity
+import controllers.gas
 
 def setup_routes(app):
     # Configure default CORS settings
@@ -14,23 +16,60 @@ def setup_routes(app):
         )
     })
 
+    # Routes
+
+    ################################################
+    # Environment
+    # Temperature
     temp_resource = cors.add(app.router.add_resource("/temperature/", name='temperature'))
     cors.add(temp_resource.add_route("GET", views.TempsView))
 
-    # TODO: Add more resources and routes
-    # cors.add(temp_resource.add_route("DELETE", remove_all_todos))
-    # cors.add(temp_resource.add_route("POST", create_todo))
     last_temp_resource = cors.add(app.router.add_resource("/temperature/last", name='last_temperature'))
     cors.add(last_temp_resource.add_route("GET", views.LastTempView))
 
+    user_temperature_log = cors.add(app.router.add_resource("/log_temperatures/", name='temperatures'))
+    cors.add(user_temperature_log.add_route("GET", controllers.temperature.log_temperatures))
+    # cors.add(user_temperature_log.add_route("DELETE", controllers.temperature.delete_temperature_log))
+
+    # Pressure
+    press_resource = cors.add(app.router.add_resource("/pressure/", name='pressure'))
+    cors.add(press_resource.add_route("GET", views.PressView))
+
+    last_press_resource = cors.add(app.router.add_resource("/pressure/last", name='last_pressure'))
+    cors.add(last_press_resource.add_route("GET", views.LastPressView))
+
+    user_pressure_log = cors.add(app.router.add_resource("/log_pressures/", name='pressures'))
+    cors.add(user_pressure_log.add_route("GET", controllers.pressure.log_pressures))
+    # cors.add(user_pressure_log.add_route("DELETE", controllers.pressure.delete_pressure_log))
+
+    # Humidity
+    humid_resource = cors.add(app.router.add_resource("/humidity/", name='humidity'))
+    cors.add(humid_resource.add_route("GET", views.HumidView))
+
+    last_humid_resource = cors.add(app.router.add_resource("/humidity/last", name='last_humidity'))
+    cors.add(last_humid_resource.add_route("GET", views.LastHumidView))
+
+    user_humidity_log = cors.add(app.router.add_resource("/log_humidity/", name='humidities'))
+    cors.add(user_humidity_log.add_route("GET", controllers.humidity.log_humidities))
+    # cors.add(user_humidity_log.add_route("DELETE", controllers.humidity.delete_humidity_log))
+
+    # Gas
+    gas_resource = cors.add(app.router.add_resource("/gas/", name='gas'))
+    cors.add(gas_resource.add_route("GET", views.GasView))
+
+    last_gas_resource = cors.add(app.router.add_resource("/gas/last", name='last_gas'))
+    cors.add(last_gas_resource.add_route("GET", views.LastGasView))
+
+    user_gas_log = cors.add(app.router.add_resource("/log_gas/", name='log_gas'))
+    cors.add(user_gas_log.add_route("GET", controllers.gas.log_gases))
+    # cors.add(user_gas_log.add_route("DELETE", controllers.gas.delete_gas_log))
+    ################################################
+
+    ################################################
+    # User
     add_user_resource = cors.add(app.router.add_resource("/user/", name='add_user'))
     cors.add(add_user_resource.add_route("POST", controllers.user.create_user))
     delete_user_resource = cors.add(app.router.add_resource("/user/", name='delete_user'))
     cors.add(delete_user_resource.add_route("DELETE", controllers.user.delete_user))
 
-    get_user_temperatures = cors.add(app.router.add_resource("/log_temperatures/", name='log_temperatures'))
-    cors.add(get_user_temperatures.add_route("GET", controllers.temperature.log_temperatures))
-    delete_user_temperatures = cors.add(app.router.add_resource("/log_temperatures/", name='delete_temperatures'))
-    cors.add(delete_user_temperatures.add_route("DELETE", controllers.temperature.delete_temperature_log))
-
-
+    # TODO: Add more resources and routes
